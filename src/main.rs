eddd6fe9af288bd177a2b2b6082435af343598ba
TJ Ayodele <timothy@freeformfuture.com>
2023-03-15 11:37:24 -0700

    inital

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::thread;
 use std::sync::mpsc;
+use serial;
 
 const VERSION: &str = "0.1";
 const BORDER_FRAME: u8 = 0x7E;
@@ -15,12 +16,10 @@ struct RS232Serial
 impl RS232Serial
 {
     // Spawns a thread that eats messages and determines if the checksum is real
-    fn start_reciever(&self, sender: mpsc::Sender<u8>) -> bool
+    fn start_reciever(&self, sender: mpsc::Sender<u8>)
     {
         // Wait to get bytes from the serial device
         // Push new message message to responder
-
-        //Spawning thread so as to ensure we grab the next frame as soon as possible
         thread::spawn(move|| {
             let test_frame: Vec<u8> = vec![0x7E, 0x0, 0x0, 0x0, 0x7E];
             loop 
@@ -28,11 +27,38 @@ impl RS232Serial
                 test_frame.iter().for_each(|byte| sender.send(*byte).expect("Failed to send byte to parser"));
             }
         });
-        true
     }
 
+    fn start_responder(&self, reciever: mpsc::Receiver<u8>)
+    {
+        // Wait to get bytes from the serial device
+        // Push new message message to responder
+
+        //Spawning thread so as to ensure we grab the next frame as soon as possible
+        thread::spawn(move|| {
+            let mut response_frame = Vec::<u8>::new();
+            let mut frame_bytes: u8;
+            loop
+            {
+                response_frame.clear();
+                frame_bytes = 0;
+                while frame_bytes < 2
+                {
+                    response_frame.push(reciever.recv().unwrap());
+                    if response_frame[response_frame.len() - 1] == BORDER_FRAME
+                    {
+                        frame_bytes += 1
+                    }
+                }
+
+                // send frame over serial tx
+                RS232Serial::send_response(&response_frame);
+            }
+        });
+    } 
+
     //send the response back across the serial device
-    fn send_response(&self, response: Vec<u8>) -> bool
+    fn send_response(response: &Vec<u8>) -> bool
     {
         response.iter().for_each(|byte| println!("Got {}", byte));
         true
@@ -47,11 +73,11 @@ enum ParseError
     InvalidFrame,
 }
 
-fn build_response(frame: Vec<u8>) -> Result<Vec<u8>, ParseError>
+fn build_response(frame: &Vec<u8>) -> Result<Vec<u8>, ParseError>
 {
     // fcs and compare with recieved frame
-    let calculated_checksum = fcs(&frame[1..(frame.len()-3)]);
-    let found_checksum: u16 = frame[(frame.len()-3)..(frame.len()-1)].iter().fold(0, |cs: u16, byte| {cs << 8; cs + (*byte as u16)});
+    let calculated_checksum = fcs(&frame[1..(frame.len()-4)]);
+    let found_checksum: u16 = frame[(frame.len()-3)..(frame.len()-2)].iter().fold(0, |cs: u16, byte| {let cs = cs << 8; cs + (*byte as u16)});
     if calculated_checksum != found_checksum 
     {
         return Err(ParseError::BadChecksum);
@@ -66,23 +92,37 @@ fn build_response(frame: Vec<u8>) -> Result<Vec<u8>, ParseError>
     Ok(vec![0x00, 0x00])
 }
 
-fn fcs(frame: &[u8]) -> u16
+// From the SPI Laser User Guide, ported to Rust
+fn fcs(command: &[u8]) -> u16
 {
-    42
+    let polynomial: u16 = 0x1081;
+    let nibble_mask: u16 = 0xF;
+    let mut temp_sum: u16;
+    let mut checksum: u16 = 0xFFFF;
+    for count in 0..command.len()
+    {
+        // subtract first nibble from checksum
+        temp_sum = (checksum ^ command[count] as u16) & nibble_mask;
+        checksum >>= 4;
+        checksum = checksum ^ (temp_sum * polynomial);
+        temp_sum = (checksum ^ ((command[count] >> 4) as u16)) & nibble_mask;
+        checksum >>= 4;
+        checksum = checksum ^ (temp_sum * polynomial);
+    }
+    // CRC finalized by inverting all the bits
+    checksum ^ 0xFFFF
 }
 
-fn main() {
-    println!("SPI Laser Emulator Version {}", VERSION);
-    let (sender, reciever) = mpsc::channel();
-    let serial = RS232Serial::default();
-    serial.start_reciever(sender);
+fn process_frames(tx_sender: mpsc::Sender<u8>, rx_reciever: mpsc::Receiver<u8>)
+{
+    let mut new_frame = Vec::<u8>::new();
     loop
     {
-        let mut frame_count = 0;
-        let mut new_frame = Vec::<u8>::new();
+        let mut frame_count: u8 = 0;
+        new_frame.clear();
         while frame_count != 2
         {
-            let new_byte: u8 = reciever.recv().unwrap();
+            let new_byte: u8 = rx_reciever.recv().unwrap();
             if new_byte == BORDER_FRAME
             {
                 frame_count += 1;
@@ -92,9 +132,24 @@ fn main() {
                 new_frame.push(new_byte);
             }
         }
-        if let Ok(response) = build_response(new_frame)
+        if let Ok(response) = build_response(&new_frame)
         {
-            serial.send_response(response);
+            response.iter().for_each(|byte| tx_sender.send(*byte).expect("Failed to send byte to responser"));
         }
     }
+}
+
+fn main() {
+    println!("SPI Laser Emulator Version {}", VERSION);
+
+    // Messages for the reciever
+    let (rx_sender, rx_reciever) = mpsc::channel();
+    // Messages for the sender
+    let (tx_sender, tx_reciever) = mpsc::channel();
+
+    let serial = RS232Serial::default();
+    serial.start_reciever(rx_sender);
+    serial.start_responder(tx_reciever);
+    // process incoming frames and, if valid, send them to the responder
+    process_frames(tx_sender, rx_reciever);
 }
